9011de22a349eb9502dc185a8e0c21f30e268c00
Anila Benny <144672603+anila-765@users.noreply.github.com>
2025-10-28 12:01:19 +0530

    Create bubble_sort.asm

---
+++ bubble_sort.asm
@@ -0,0 +1,86 @@
+.model small
+
+.data
+n dw ?
+arr db 30 dup(?)
+m1 db 10,13,"Enter the array $"
+m3 db 10,13,"sorted array $"
+
+.code
+mov ax,@data
+mov ds,ax
+
+lea dx,m1
+mov ah,09h
+int 21h
+
+mov si,0
+
+read:
+mov ah,01h
+int 21h
+cmp al,0dh
+je start
+
+sub al,30h
+mov arr[si],al
+inc si
+jmp read
+
+start:
+mov n,si
+dec si
+mov di,si
+
+go:
+cmp di,0
+je sorted
+
+mov si,0
+
+compare:
+cmp si,di
+jge nextloop
+
+mov al,arr[si]
+mov bl,arr[si+1]
+cmp al,bl
+jle next
+
+mov arr[si],bl
+mov arr[si+1],al
+inc si
+
+jmp compare
+
+next:
+inc si
+jmp compare
+
+nextloop:
+dec di
+jmp go
+
+sorted:
+lea dx,m3
+mov ah,09h
+int 21h
+
+mov cx,n
+mov si,0
+
+print:
+
+mov al,arr[si]
+inc si
+
+add al,30h
+mov dl,al
+mov ah,02h
+int 21h
+
+loop print
+
+mov ah,4ch
+int 21h
+end
